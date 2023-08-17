3ae7041cb12835772defb9be3b26d5fbfaf5853e
vlakhara <vipullakhara18@gmail.com>
2023-08-17 23:01:38 +0530

    remove consoles

--- index.js
+++ index.js
@@ -23,13 +23,10 @@ io.on("connection", (socket) => {
 
   socket.on("send_message", (messageData) => {
     socket.to(messageData.room).emit("message_received", messageData);
-    console.log("Message Data: ", messageData);
   });
   socket.on("disconnect", () => {
     console.log("User with ID disconnected: ", socket.id);
   });
 });
 
-server.listen(3001, () => {
-  console.log("Connected");
-});
+server.listen(3001, () => {});
